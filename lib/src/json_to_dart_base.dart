4e0e6ea800b7294716603503022a5b4ee0aa50c6
eyad.alsawah <eyad.alsawah@nasca-sy.com>
2023-09-22 17:34:20 +0300

    waiting for the user to enter path instead of taking it as a param

--- lib/src/json_to_dart_base.dart
+++ lib/src/json_to_dart_base.dart
@@ -1,19 +1,25 @@
-// TODO: Put public facing types in this file.
-
 import 'dart:io';
 
 import 'package:json_to_dart/src/json_to_dart/helper_methods.dart';
-
 import 'json_to_dart/classes_to_json.dart';
 import 'json_to_dart/json_to_class.dart';
 import 'json_to_dart/json_to_dart.dart';
 
-void convertJsonToDart(
-    {required String jsonFilePath,
-    required String outputPath,
-    ConverterOptions? converterOptions}) async {
+void convertJsonToDart({ConverterOptions? converterOptions}) async {
+  print('Enter the path to your JSON file: ');
+  final String jsonFilePath = stdin.readLineSync() ?? '';
+
+  print('Enter the path for the output Dart file: ');
+  final String outputPath = stdin.readLineSync() ?? '';
+
   final File jsonFile = File(jsonFilePath);
   final File outputFile = File(outputPath);
+
+  if (!jsonFile.existsSync()) {
+    print('JSON file not found at the specified path.');
+    return;
+  }
+
   final jsonString = await jsonFile.readAsString();
 
   String fileToGenerate = '';
